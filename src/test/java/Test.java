ffc518e4d3e84acb5ef37cad8f640e467632dd54
Tatjanas1 <tatiana.svistunova1@googlemail.com>
2022-05-10 15:20:33 +0300

    Edward doesn't work, own Test doesn't work

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,28 +4,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
-public class EdwardTest extends BaseUtils{
+public class Test {
+
     WebDriver driver = new ChromeDriver();
 
     @BeforeMethod
     public void Base() {
-        driver.get("https://www.google.com/");
+        System.setProperty("webdriver.chrome.driver","/home/vitat/Downloads/chromedriver");
+        driver.get("https://www.geonames.org/");
         driver.manage().window().fullscreen();
         driver.manage().deleteAllCookies();
     }
 
-    @Test
-    public void Test() throws AWTException {
+    @org.testng.annotations.Test
+    public void Test() {
 
         driver.findElement(By.name("q")).sendKeys("Selenium");
-        Robot r = new Robot();
-        r.keyPress(KeyEvent.VK_ENTER);
-//это почему-то не работает...
+        WebElement searchButton = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
 
 
     }
@@ -34,6 +32,5 @@ public class EdwardTest extends BaseUtils{
     void Close(){
         driver.close();
     }
+
 }
-
-
